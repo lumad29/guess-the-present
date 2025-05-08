84ad005c48528a11a413563be25dc8bf5f369580
Lucrecia Aloi <lucreciaaloi@MacBook-Pro-de-Lucrecia.local>
2025-05-07 21:18:46 -0300

    updated footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,39 +3,18 @@
     app
     height="40"
   >
-    <a
-      v-for="item in items"
-      :key="item.title"
-      class="d-inline-block mx-2 social-link"
-      :href="item.href"
-      rel="noopener noreferrer"
-      target="_blank"
-      :title="item.title"
-    >
-      <v-icon
-        :icon="item.icon"
-        :size="item.icon === '$vuetify' ? 24 : 16"
-      />
-    </a>
+
 
     <div
       class="text-caption text-disabled"
       style="position: absolute; right: 16px;"
     >
-      &copy; 2016-{{ (new Date()).getFullYear() }} <span class="d-none d-sm-inline-block">Vuetify, LLC</span>
-      —
-      <a
-        class="text-decoration-none on-surface"
-        href="https://vuetifyjs.com/about/licensing/"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        MIT License
-      </a>
+      &copy; 2025 <span>  Para Vitaly de Lucrecia  </span>
+    
     </div>
   </v-footer>
 </template>
-
+<!-- 
 <script setup>
   const items = [
     {
@@ -69,7 +48,7 @@
       href: 'https://reddit.com/r/vuetifyjs',
     },
   ]
-</script>
+</script> -->
 
 <style scoped lang="sass">
   .social-link :deep(.v-icon)
